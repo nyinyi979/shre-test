b2963a722c43a39f50b06dd8c201a21765736d30
nyinyi979 <nyinyi095062687@gmail.com>
2024-05-30 13:38:54 +0630

    update v1.4

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from "next";
-import ClientHeader from "./_components/clientHeader";
-import ClientLegend from "./_components/clientLegend";
-import FAQs from "./_components/faq";
-import Features from "./_components/features";
-import GetStarted from "./_components/getStarted";
-import SignatureProducts from "./_components/signatureProducts";
+import ClientHeader from "../_components/_components/clientHeader";
+import ClientLegend from "../_components/_components/clientLegend";
+import FAQs from "../_components/_components/faq";
+import Features from "../_components/_components/features";
+import GetStarted from "../_components/_components/getStarted";
+import SignatureProducts from "../_components/_components/signatureProducts";
 import HowItWorks from "../_components/howItWorks";
 
 export default function Client(){
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,11 +1,11 @@
 import { Metadata } from "next";
-import SignatureProducts from "../client/_components/signatureProducts";
+import SignatureProducts from "../_components/_components/signatureProducts";
 import CostEffective from "./_components/costEffective";
 import SeamlessCRM from "./_components/crm";
 import DigitalBusiness from "./_components/digitalBusiness";
 import HowItWorks from "../_components/howItWorks";
 import LeadToDeal from "./_components/leadToDeal";
-import Plan from "../_components/plan";
+import Plan from "./_components/plan";
 import WasteLess from "./_components/wasteLess";
 export default function(){
     return(
